f54be3dab801a0339c06999581490ba5439a90df
abuabdelrahman1413 <ms22abuabdelrahman@gmail.com>
2024-10-28 02:37:14 +0200

    lazyvim

---
+++ nvim-lazyvim/lua/plugins/live-server.lua
@@ -0,0 +1,13 @@
+return {
+  "barrett-ruth/live-server.nvim",
+  config = function()
+    require("live-server").setup({
+      settings = {
+        log = false,
+        open = false,
+        wait = 1000,
+        ignore = "scss,less,config.js,md,txt",
+      },
+    })
+  end,
+}
