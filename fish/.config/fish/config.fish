52a2af151ec9686428abd4819dda0629c6367bdd
mohammed <mohammed@ubuntu-mate24.10>
2024-12-27 18:51:14 +0200

    up

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -11,3 +11,12 @@ alias upgrade="sudo apt upgrade"
 alias install="sudo apt install"
 alias list="apt list --upgradeable"
 alias i3="nvim ~/dotfiles/i3/.config/i3/config"
+source "$HOME/.cargo/env.fish"
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
